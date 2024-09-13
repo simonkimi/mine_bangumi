9b7cab3124207bf166627334c93010e9aae6febe
simonkimi <git@z31.mozmail.com>
2024-09-13 22:21:12 +0800

    优化日志

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -10,6 +10,25 @@ import (
 	"time"
 )
 
+type formatterHook struct {
+	writer    io.Writer
+	formatter logrus.Formatter
+	levels    []logrus.Level
+}
+
+func (f *formatterHook) Fire(entry *logrus.Entry) error {
+	data, err := f.formatter.Format(entry)
+	if err != nil {
+		return err
+	}
+	_, err = f.writer.Write(data)
+	return err
+}
+
+func (f *formatterHook) Levels() []logrus.Level {
+	return f.levels
+}
+
 func Setup() {
 	startUpTimeStr := time.Now().Format("2006-01-02-150405")
 	wd, err := os.Getwd()
@@ -17,14 +36,24 @@ func Setup() {
 		panic(err)
 	}
 	loggerFile := filepath.Join(wd, "logs", fmt.Sprintf("%s.log", startUpTimeStr))
-	mw := io.MultiWriter(os.Stdout, &lumberjack.Logger{
-		Filename:   loggerFile,
-		MaxBackups: 7,
-		MaxAge:     30,
-		LocalTime:  true,
-		Compress:   true,
+
+	fileHook := &formatterHook{
+		writer: &lumberjack.Logger{
+			Filename:   loggerFile,
+			MaxBackups: 7,
+			MaxAge:     30,
+			LocalTime:  true,
+			Compress:   true,
+		},
+		formatter: &logrus.JSONFormatter{},
+		levels:    logrus.AllLevels,
+	}
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
 	})
+	logrus.AddHook(fileHook)
 	logrus.SetLevel(logrus.DebugLevel)
-	logrus.SetOutput(mw)
-	logrus.SetFormatter(&logrus.TextFormatter{})
 }
